20775481529adba2d5f01a3e10d25f7661acd39d
Olivia Wyand <owyand22@gmail.com>
2024-02-16 09:41:00 -0500

    model is filled correctly. now able to access events for all sports

--- Arbitrage/Models/EventModel.cs
+++ Arbitrage/Models/EventModel.cs
@@ -1,12 +1,25 @@
-﻿namespace Arbitrage.Models
+﻿using Newtonsoft.Json;
+
+namespace Arbitrage.Models
 {
     public class EventModel
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [JsonProperty("sport_key")]
         public string SportKey { get; set; }
+
+        [JsonProperty("sport_title")]
         public string SportTitle { get; set; }
-        public DateTime CommenceTime { get; set; }
+
+        [JsonProperty("commence_time")]
+        public string CommenceTime { get; set; }
+
+        [JsonProperty("home_team")]
         public string HomeTeam { get; set; }
+
+        [JsonProperty("away_team")]
         public string AwayTeam { get; set; }
 
     }
