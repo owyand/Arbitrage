508cb50ee32e93e2ea87b5ff9fcff377902cda23
Olivia Wyand <owyand22@gmail.com>
2024-02-20 10:49:55 -0500

    roughed in a bookmaker page created a bookmaker controller with a new view folder and index page

---
+++ Arbitrage/Controllers/BookmakerController.cs
@@ -0,0 +1,15 @@
+﻿using Arbitrage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arbitrage.Controllers
+{
+
+    //this controller doesnt need any api calls it is all passed through from odds details
+    public class BookmakerController : Controller
+    {
+        public IActionResult Index(IEnumerable<BookmakerModel> bookmakers)
+        {
+            return View(bookmakers);
+        }
+    }
+}
