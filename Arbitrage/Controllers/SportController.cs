7b346fa2796412eae9310a2643ba51daf8917b97
Olivia Wyand <owyand22@gmail.com>
2024-03-11 18:28:30 -0400

    made the changes i needed. used TempData instead of serializing and deserializing what i needed to pass by hand. moved made bookmaker/index into sport/bookmakerdetails to keep it in the same controller as everything else

--- Arbitrage/Controllers/SportController.cs
+++ Arbitrage/Controllers/SportController.cs
@@ -1,6 +1,7 @@
 ﻿using Arbitrage.Models;
 using Arbitrage.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Arbitrage.Controllers
 {
@@ -38,16 +39,55 @@ namespace Arbitrage.Controllers
 
         public async Task<IActionResult> OddsDetails(string sportKey)
         {
+
             IEnumerable<OddsModel> sportOdds = await _sportsbookApiService.GetOdds(sportKey);
+
+            try
+            {
+                TempData["gameList"] = JsonConvert.SerializeObject(sportOdds);
+
+                TempData.Keep("gameList");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
             if (sportOdds == null)
             {
                 Console.WriteLine("sportEvents is null");
             }
             else
             {
+                //get the header for request displayed here
                 Console.WriteLine("sportEvents is not null");
             }
             return View(sportOdds);
+        }
+
+        public async Task<IActionResult> BookmakerDetails(string id)
+        {
+
+            // Retrieve the JSON string from TempData and deserialize it
+            var gameListJson = TempData["gameList"] as string;
+            var gameList = JsonConvert.DeserializeObject<IEnumerable<OddsModel>>(gameListJson);
+
+            if (gameList == null)
+            {
+                throw new Exception("API call is null for GetOdds. Possible TempData serialization issues");
+            }
+            else
+            {
+                foreach (var game in gameList)
+                {
+                    if (game.Id == id)
+                    {
+                        return View(game);
+                    }
+                }
+            }
+            return View();
         }
 
         //find list of events for that sport
